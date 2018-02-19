3f6ab2fc0f57ffe79a76fad8964b2cbb8f13fa51
Teodorek <36641881+Teodorek@users.noreply.github.com>
2018-02-19 19:13:54 +0100

    3

---
+++ czlowiek/src/czlowiek/Czlowiek.java
@@ -0,0 +1,19 @@
+
+package czlowiek;
+import klasa.klasaCzlek;
+
+public class Czlowiek {
+
+
+    public static void main(String[] args) {
+     
+        
+        klasaCzlek Marta = new klasaCzlek();
+        
+        Marta.setImie("Marta");
+        
+        System.out.println(Marta.getImie());
+        System.out.println(Marta.nazwisko);
+    }
+    
+}
---
+++ czlowiek/src/klasa/klasaCzlek.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package klasa;
+
+
+public class klasaCzlek {
+
+        public String imie;
+        public String nazwisko;
+    
+    public klasaCzlek() 
+    {    
+    nazwisko = "Teodorowicz";
+    }   
+
+    public String getImie()
+     {
+        return imie;
+     }
+     
+    public void setImie(String imie)
+     {
+         this.imie = imie;
+     }
+
+    
+    
+        
+}
